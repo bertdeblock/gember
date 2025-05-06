0fcf15322225dd5eb38d26578de393593120f557
Bert De Block <b.deblock@codeshare.be>
2025-05-06 13:48:31 +0200

    Remove `helper` wrapper from `helper` generator

--- templates/helper/function-based.ts
+++ templates/helper/function-based.ts
@@ -1,17 +1,3 @@
-import { helper } from "@ember/component/helper";
-
-type Named = {};
-type Positional = [];
-type Return = Positional;
-
-export interface {{name.signature}} {
-  Args: {
-    Named: Named;
-    Positional: Positional;
-  };
-  Return: Return;
-}
-
-export default helper<{{name.signature}}>(function {{name.camel}}(positional, named) {
+export default function {{name.camel}}(positional, named) {
   return positional;
-});
+}
