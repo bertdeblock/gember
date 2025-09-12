bfe6fa2e0d1ad36994c89323ce90b5ebf487b445
Bert De Block <b.deblock@codeshare.be>
2025-09-12 17:37:31 +0200

    Run smoke test in CI

--- src/generator.ts
+++ src/generator.ts
@@ -60,12 +60,12 @@ export function defineGenerator({
   name,
 }: GeneratorOptions): Generator {
   const generatorName = name;
-  const generatorArgs = [copy(), log(), ...args]
+  const generatorArgs = [copy(), cwd(), log(), ...args]
     .map((argFactory) => argFactory(generatorName))
     .sort((a, b) => a.name.localeCompare(b.name));
 
   async function run(args: Args): Promise<void> {
-    const packagePath = cwd();
+    const packagePath = args.cwd ?? processCwd();
     const packageJson: EmberPackageJson = await readJson(
       join(packagePath, "package.json"),
     );
@@ -249,6 +249,14 @@ export function copy(): GeneratorArgFactory {
     description: `Copy the generated ${generatorName} to the clipboard, instead of writing it to disk`,
     name: "copy",
     type: "boolean",
+  });
+}
+
+export function cwd(): GeneratorArgFactory {
+  return () => ({
+    description: "The current working directory to run the generator in",
+    name: "cwd",
+    type: "string",
   });
 }
 
